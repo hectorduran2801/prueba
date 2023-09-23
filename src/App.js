86ea4238890e83f4d077d17a73aa76abd3729ea4
hectorduran2801 <hduranc24@gmail.com>
2023-09-23 17:12:55 -0500

    first committ

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [response, setResponse] = useState(null);
+
+  const handleClick = async () => {
+    const url = "https://appointment.ena.com.pa/api/Informacion/recibir";
+    const data = {
+      Informacion: {
+        Name: "vane",
+        Apellido: "sarmiento",
+        Cedula: "171985331",
+        Email: "hduranc24@gmail.com",
+        Celular: "69271957",
+        Telefono: "69271957",
+        Placa: "MI2001",
+        Corregimiento: "NA",
+        Edificio: "NA",
+        Calle: "NA",
+        Casa: "NA",
+        Tag: "NA",
+        Color: "NA",
+        Cuenta: "NA",
+        Modelo: "NA",
+        Stiker: "NA",
+        ValorJson: "NA",
+        PolizaAuto: "NA",
+        RazonSocial: "NA",
+        CopiaCedulaJ: "NA",
+        CopiaCedulaN: "NA",
+        ValorJsonOld: "NA",
+        AvisoOperacion: "NA",
+        NombreComercial: "NA",
+        RegistroVehicular: "NA",
+        RepresentanteLegal: "NA",
+        CopiaAvisoOperacion: "NA",
+        DocumentoAutorizacion: "NA",
+      },
+      Comentario: {
+        Mensaje: "esto fue vanessa desde azure",
+      },
+    };
+
+    const config = {
+      auth: {
+        username: "gtena3tech",
+        password: "gtena3tech2023..",
+      },
+    };
+
+    try {
+      const response = await axios.post(url, data, config);
+      setResponse(response.data);
+    } catch (error) {
+      console.error("Error al enviar esta vaina:", error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick}>Enviar Solicitud</button>
+        {response && (
+          <div>
+            <p>Respuesta del servidor:</p>
+            <pre>{JSON.stringify(response, null, 2)}</pre>
+          </div>
+        )}
       </header>
     </div>
   );
